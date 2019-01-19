25ef8ce6a35e620d8373d808f8033dcc5b2a75cf
李建宗 <318717449@qq.com>
2019-01-20 00:36:17 +0800

    index

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -2,9 +2,18 @@ package com.example.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsApplication {
+
+    @RequestMapping("")
+    public String index() {
+
+        return "Jenkins";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsApplication.class, args);
